f16d4fa212271acafe13bfa20139f0e6a157b747
jeeheaG <jeehea99@naver.com>
2021-10-04 21:22:50 +0900

    9372 해결

--- 04_tree,priorityQueue/9372_상근이의여행.py
+++ 04_tree,priorityQueue/9372_상근이의여행.py
@@ -1,2 +1,21 @@
 # 백준 9372 상근이의 여행 트리
 from sys import stdin
+
+#입력
+T = int(stdin.readline())
+#중첩 리스트 두 개 - 각 케이스의 나라&비행기수, 비행기목록
+# course = [0]*T
+course = []
+airplane = []
+for i in range(T) : 
+    course.append(list(map(int, stdin.readline().split())))
+    t = []
+    for j in range(course[i][1]) :
+        t.append(list(map(int, stdin.readline().split())))
+    airplane.append(t)
+# print(course)
+# print(airplane)
+
+#연결리스트임 - 모든 비행기가 연결되어있으므로 그냥 방문해야 하는 나라 - 1 하면 됨
+for i in range(T) :
+    print(course[i][0]-1)
