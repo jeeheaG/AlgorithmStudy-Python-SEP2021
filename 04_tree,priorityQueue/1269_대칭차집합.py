74ef601a16f8730a6a5ca5de78e79221444b2447
jeeheaG <jeehea99@naver.com>
2021-10-04 20:42:19 +0900

    1269 해결_1

--- 04_tree,priorityQueue/1269_대칭차집합.py
+++ 04_tree,priorityQueue/1269_대칭차집합.py
@@ -1,2 +1,10 @@
 # 백준 1269 대칭 차집합 트리
+# 집합 자료형 사용하지 않고도 풀어보기
 from sys import stdin
+
+NA, NB = map(int, stdin.readline().split())
+#집합 자료형 set()
+A = set(list(map(int, stdin.readline().split())))
+B = set(list(map(int, stdin.readline().split())))
+
+print(len(A - B) + len(B - A))
